83d5955bfad6cbee1365b8cb3c9584af121f6c3a
PiotMac <268425@student.pwr.edu.pl>
2025-05-28 17:25:38 +0000

    Another try

--- infra/step-function.tf
+++ infra/step-function.tf
@@ -4,6 +4,5 @@ module "workflow" {
     role_arn = data.aws_iam_role.main_role.arn
     definition = "workflow.json"
     grouper_lambda_arn    = module.grouper_lambda.lambda_function_arn
-    # error_handling_lambda_arn = module.error_handling_lambda.lambda_function_arn
     aggregator_lambda_arn    = module.aggregator_lambda.lambda_function_arn
 }
